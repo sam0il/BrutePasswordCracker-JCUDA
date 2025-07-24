86c3df785056d36cfce8edb0a83e9608bb9107eb
sam0il <samoilgorgiev@yahoo.com>
2025-07-24 11:12:55 +0200

    final strech doing the DictionaryAttack.

---
+++ src/main/java/org/example/JCudaDictionaryEngine.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import jcuda.Pointer;
+import jcuda.Sizeof;
+import jcuda.driver.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import static jcuda.driver.JCudaDriver.*;;
+
+public class JCudaDictionaryEngine {
+}
